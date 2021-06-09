d7089c3812ee30a2e20df78e5f48c4ce445caca8
Piotrek Wielki <remmington.cruzoe@gmail.com>
2021-06-09 12:11:24 +0200

    first commit

---
+++ js/my.js
@@ -0,0 +1,8 @@
+// Create variables (fullName, phone, hobbies, email, websiteLink, websiteTitle) after this comment
+// what type should you use if this variables will not be changed?
+
+/**
+ *  Remove '//' before render(.... to uncomment function and make it work
+ */
+
+//  render(fullName, phone, hobbies, email, websiteLink, websiteTitle);
