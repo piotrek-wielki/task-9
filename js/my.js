e140126292cae993fd2149be4d24c137b10160cc
Piotrek Wielki <remmington.cruzoe@gmail.com>
2021-06-09 17:19:25 +0200

    boolean

--- js/my.js
+++ js/my.js
@@ -4,7 +4,7 @@
 const fullName = "Piotrek Wielki";
 const phone = "66-66";
 const hobbies = "killing it... in IT:D";
-const email = "allegeditkiller@gmail.com";
+const email = false;
 const websiteLink = "www.allegeditkiller.com";
 const websiteTitle = "Killer IT Stories";
 
